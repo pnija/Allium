88f2bcb6a9e7fb78be97ae3d0e43d642e6e037da
salam <abdul.salam@techversantinc.com>
2018-04-19 14:52:14 +0530

    static fix

--- Allium/settings.py
+++ Allium/settings.py
@@ -139,7 +139,7 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/staticfiles/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
 
 # For email
