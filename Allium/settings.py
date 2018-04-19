84bafe096d7ef3d33755d401a16c047181c48185
salam <abdul.salam@techversantinc.com>
2018-04-19 18:13:15 +0530

    changes

--- Allium/settings.py
+++ Allium/settings.py
@@ -93,7 +93,6 @@ if DB_URL != False:
     db_from_env = dj_database_url.config()
     DATABASES['default'].update(db_from_env)
     DEBUG = False
-    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 else:
     print("-----DEBUG------")
     DATABASES = {
@@ -103,15 +102,15 @@ else:
         }
     }
     DEBUG = True
-    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+    
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
