80c7a44dde44719d42e4dfee5682847ccbdcdeb0
salam <abdul.salam@techversantinc.com>
2018-04-19 14:41:19 +0530

    staticfiles change

--- Allium/settings.py
+++ Allium/settings.py
@@ -139,8 +139,8 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-STATICFILES_DIRS = (os.path.join(PROJECT_DIR, 'staticfiles'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
 
 # For email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
