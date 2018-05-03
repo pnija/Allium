2d06d0089fb6e3533ffd429bf981ede869e32bea
salam <abdul.salam@techversantinc.com>
2018-05-03 11:52:20 +0530

    changed custom permission classes

---
+++ api/custom_permissions/permissions.py
@@ -0,0 +1,52 @@
+from rest_framework import permissions
+from django.contrib.auth.models import Group
+
+
+class PartnerAccessPermission(permissions.BasePermission):
+	message = "Only 'Partner' allowed to access this api."
+	
+	def is_member(self, user):
+		return user.groups.filter(name='Partner').exists()
+
+	def has_permission(self, request, view):
+		"""
+		Return `True` if permission is granted, `False` otherwise.
+		"""	
+		if not request.user.is_anonymous:
+			if self.is_member(request.user):
+				return True
+		return False
+
+	def has_object_permission(self, request, view, obj):
+		"""
+		Return `True` if permission is granted, `False` otherwise.
+		"""
+		if not request.user.is_anonymous:
+			if self.is_member(request.user):
+				return True
+		return False
+
+
+class AdminAccessPermission(permissions.BasePermission):
+	message = "Only 'Admin' allowed to access this api."
+	
+	def is_admin(self, user): 
+		return user.groups.filter(name='Admin').exists()
+
+	def has_permission(self, request, view):
+		"""
+		Return `True` if permission is granted, `False` otherwise.
+		"""		
+		if not request.user.is_anonymous:
+			if self.is_admin(request.user):
+				return True
+		return False
+
+	def has_object_permission(self, request, view, obj):
+		"""
+		Return `True` if permission is granted, `False` otherwise.
+		"""
+		if not request.user.is_anonymous:
+			if self.is_admin(request.user):
+				return True
+		return False
